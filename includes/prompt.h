4532c12c6262646c323cb3b456cb6344f8ecdb59
urassh <shuto.1011@icloud.com>
2025-10-02 11:30:00 +0900

    libftの相対パス指定を修正

--- includes/prompt.h
+++ includes/prompt.h
@@ -6,16 +6,16 @@
 /*   By: urassh <urassh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:35:18 by urassh            #+#    #+#             */
-/*   Updated: 2025/10/01 16:50:03 by urassh           ###   ########.fr       */
+/*   Updated: 2025/10/02 11:29:43 by urassh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROMPT_H
 # define PROMPT_H
 
-# include "../libft/libft.h"
-# include <readline/readline.h>
+# include "libft.h"
 # include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
