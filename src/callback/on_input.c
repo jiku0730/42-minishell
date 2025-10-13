1c3576ab2d5e95a48e04d1865f474fd2d0b2005e
urassh <shuto.1011@icloud.com>
2025-10-13 14:21:38 +0900

    fix: on_exitのシグネチャ被りのビルドエラーを修正

--- src/callback/on_input.c
+++ src/callback/on_input.c
@@ -6,13 +6,13 @@
 /*   By: urassh <urassh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:04:25 by urassh            #+#    #+#             */
-/*   Updated: 2025/10/13 01:00:00 by urassh           ###   ########.fr       */
+/*   Updated: 2025/10/13 14:20:53 by urassh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	on_exit(t_list *token_list);
+static void	on_exit_token(t_list *token_list);
 static void	print_tokens(t_list *token_list);
 
 void	on_input(char *input)
@@ -28,7 +28,7 @@ void	on_input(char *input)
 		return ;
 	}
 	if (token_list->content && ft_strncmp(token_list->content, "exit", 5) == 0)
-		on_exit(token_list);
+		on_exit_token(token_list);
 	print_tokens(token_list);
 	ft_lstclear(&token_list, free);
 }
@@ -47,15 +47,14 @@ static void	print_tokens(t_list *token_list)
 		ft_putnbr_fd(index, STDOUT_FILENO);
 		write(STDOUT_FILENO, "] \"", 3);
 		if (current->content)
-			write(STDOUT_FILENO, current->content,
-				ft_strlen(current->content));
+			write(STDOUT_FILENO, current->content, ft_strlen(current->content));
 		write(STDOUT_FILENO, "\"\n", 2);
 		current = current->next;
 		index++;
 	}
 }
 
-static void	on_exit(t_list *token_list)
+static void	on_exit_token(t_list *token_list)
 {
 	write(STDOUT_FILENO, "exit\n", 5);
 	ft_lstclear(&token_list, free);
