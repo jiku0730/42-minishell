8509ab5c0c3e79ce42732dbf27d1f6bd1251daf2
urassh <shuto.1011@icloud.com>
2025-10-22 12:44:10 +0900

    fix: rtv_tokens

--- src/tokenize/state/on_success.c
+++ src/tokenize/state/on_success.c
@@ -6,7 +6,7 @@
 /*   By: urassh <urassh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 00:35:00 by urassh            #+#    #+#             */
-/*   Updated: 2025/10/17 22:50:26 by urassh           ###   ########.fr       */
+/*   Updated: 2025/10/22 12:44:01 by urassh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 t_list	*on_success(t_token_store *store, char *input)
 {
-	t_list	*tokens;
+	t_list	*rtv_tokens;
 
 	if (!store)
 		return (NULL);
 	if (input)
 		free(input);
-	tokens = store->tokens;
+	rtv_tokens = store->tokens;
 	store->tokens = NULL;
 	if (store->buffer)
 		ft_lstclear(&(store->buffer), free);
-	return (tokens);
+	return (rtv_tokens);
 }
