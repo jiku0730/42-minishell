3a275bdb4c897fad1d30d3937904fce3c454c54b
urassh <shuto.1011@icloud.com>
2025-10-23 01:00:54 +0900

    in_operatorをもうちょいわかりやすく

--- src/tokenize/state/in_operator.c
+++ src/tokenize/state/in_operator.c
@@ -12,15 +12,41 @@
 
 #include "tokenize.h"
 
+static void by_operator(t_token_store *store, t_token_state *state,
+		const char current);
+static void by_normal(t_token_store *store, t_token_state *state,
+		const char current);
+
 void	in_operator(t_token_store *store, t_token_state *state,
 		const char current)
 {
 	if (current == '\0')
 		*state = ON_ERROR;
-	else if (is_operator_char(current) && add_buffer(store, current) == ERROR)
+	else if (is_operator_char(current))
+		by_operator(store, state, current);
+	else
+		by_normal(store, state, current);
+}
+
+static void	by_operator(t_token_store *store, t_token_state *state,
+		const char current)
+{
+	if (add_buffer(store, current) == ERROR)
 		*state = ON_ERROR;
 	else if (push_token(store) == ERROR)
 		*state = ON_ERROR;
 	else
 		*state = IN_NORMAL;
+}
+
+static void	by_normal(t_token_store *store, t_token_state *state,
+		const char current)
+{
+	if (push_token(store) == ERROR)
+		*state = ON_ERROR;
+	else
+	{
+		*state = IN_NORMAL;
+		in_normal(store, state, current);
+	}
 }
