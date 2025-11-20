3ea13fc9c25609a93f9a2912f498a316bce445a5
jiku0730 <kei2003730@gmail.com>
2025-11-20 12:04:47 +0900

    revise: norminette

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 16:38:39 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/11/20 11:50:13 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2025/11/20 11:57:10 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,39 +58,39 @@ typedef struct s_ast
 }					t_ast;
 
 /* main function------------------------------------------------------------- */
-t_ast				*parser(t_list *head);
+t_ast			*parser(t_list *head);
 
 /* AST's token utils--------------------------------------------------------- */
-bool				is_word(const t_list *node);
-bool				is_symbol(const t_list *node, const char *literal);
-bool				is_eof(const t_list *node);
-bool				is_redir(const t_list *node);
-const char			*get_content(const t_list *node);
+bool			is_word(const t_list *node);
+bool			is_symbol(const t_list *node, const char *literal);
+bool			is_eof(const t_list *node);
+bool			is_redir(const t_list *node);
+const char		*get_content(const t_list *node);
 
 /* AST's Utils--------------------------------------------------------------- */
-t_ast				*new_ast_node(t_ast_type type);
-t_cmd				*new_cmd(void);
-t_redir				*new_redir(t_redir_kind kind, const char *filename);
-bool				add_redir_to_cmd(t_cmd *cmd, t_redir_kind kind,
-						const char *file);
-bool				add_argv_to_cmd(t_cmd *cmd, const char *arg);
+t_ast			*new_ast_node(t_ast_type type);
+t_cmd			*new_cmd(void);
+t_redir			*new_redir(t_redir_kind kind, const char *filename);
+bool			add_redir_to_cmd(t_cmd *cmd, t_redir_kind kind,
+					const char *file);
+bool			add_argv_to_cmd(t_cmd *cmd, const char *arg);
 
 /* free utils function------------------------------------------------------- */
-void				free_cmd(t_cmd *cmd);
-void				free_ast(t_ast *node);
+void			free_cmd(t_cmd *cmd);
+void			free_ast(t_ast *node);
 
 /* test print function------------------------------------------------------- */
 void			print_branch_line(const char *prefix, bool is_last,
-				const char *text);
+					const char *text);
 void			build_child_prefix(const char *prefix, bool is_last,
-				char *buffer, size_t size);
+					char *buffer, size_t size);
 void			print_argv_item(t_list *node, int index);
 const char		*get_redir_symbol(t_redir *redir);
 void			print_redir_entry(t_redir *redir, int index);
 void			print_argv_line(const char *prefix, bool is_last,
-				t_list *argv);
+					t_list *argv);
 void			print_redir_line(const char *prefix, bool is_last,
-				t_list *redirs);
+					t_list *redirs);
 void			print_pipe_children(t_ast *node, const char *prefix);
 void			print_pipe_node(t_ast *node, const char *prefix, bool is_last);
 void			print_ast_node(t_ast *node, const char *prefix, bool is_last);
