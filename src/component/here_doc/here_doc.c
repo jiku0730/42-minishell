5c1878770b82341f9f358ef9c5ad7c4c000af40d
urassh <shuto.1011@icloud.com>
2025-11-20 15:46:03 +0900

    feat: heredoc関数を実装した

--- src/component/here_doc/here_doc.c
+++ src/component/here_doc/here_doc.c
@@ -12,6 +12,45 @@
 
 #include <here_doc.h>
 
+static bool	is_heredoc_operator(const char *token);
+static void rebuild_tokens_with_heredoc(t_list *current, t_list *heredoc_node, t_list *eof_node);
+
 t_list	*here_doc(t_list *tokens)
 {
+	t_list	*current;
+	t_list	*eof_node;
+	t_list	*heredoc_node;
+
+	current = tokens;
+	while (current != NULL)
+	{
+		if (is_heredoc_operator((char *)current->content))
+		{
+			eof_node = current->next;
+			if (eof_node == NULL)
+				return (NULL);
+			heredoc_node = here_doc_prompt((char *)eof_node->content);
+			if (heredoc_node == NULL)
+				return (NULL);
+            rebuild_tokens_with_heredoc(current, heredoc_node, eof_node);
+		}
+		current = current->next;
+	}
+	return (tokens);
+}
+
+static bool	is_heredoc_operator(const char *token)
+{
+	if (token == NULL)
+		return (false);
+	return (ft_strncmp(token, "<<", 3) == 0);
+}
+
+static void rebuild_tokens_with_heredoc(t_list *current, t_list *heredoc_node, t_list *eof_node)
+{
+    current->content = heredoc_node->content;
+    current->next = eof_node->next;
+    free(eof_node->content);
+    free(eof_node);
+    free(heredoc_node);
 }
