b6e9fec3ef64ac33575d971dcbf30cafb64bfe28
urassh <shuto.1011@icloud.com>
2025-11-23 15:19:03 +0900

    fix: ht_createでcallocを使うように

--- libft/hash_table/hash_table.c
+++ libft/hash_table/hash_table.c
@@ -15,14 +15,13 @@
 t_hash_table	*ht_create(size_t size)
 {
 	t_hash_table	*table;
-	size_t			i;
 
 	if (size == 0)
 		return (NULL);
-	table = (t_hash_table *)malloc(sizeof(t_hash_table));
+	table = (t_hash_table *)ft_calloc(1, sizeof(t_hash_table));
 	if (!table)
 		return (NULL);
-	table->buckets = (t_hash_node **)malloc(sizeof(t_hash_node *) * size);
+	table->buckets = (t_hash_node **)ft_calloc(size, sizeof(t_hash_node *));
 	if (!table->buckets)
 	{
 		free(table);
@@ -30,11 +29,5 @@ t_hash_table	*ht_create(size_t size)
 	}
 	table->size = size;
 	table->n_nodes = 0;
-	i = 0;
-	while (i < size)
-	{
-		table->buckets[i] = NULL;
-		i++;
-	}
 	return (table);
 }
