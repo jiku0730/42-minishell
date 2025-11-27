3b6cf4fa57737d1291bc219f3759f2fd1a1238bc
urassh <shuto.1011@icloud.com>
2025-11-27 16:06:23 +0900

    feaet: get_directory_entriesを再帰で読み取るように

--- includes/directory.h
+++ includes/directory.h
@@ -6,17 +6,22 @@
 /*   By: surayama <surayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 15:25:07 by surayama          #+#    #+#             */
-/*   Updated: 2025/11/27 15:30:16 by surayama         ###   ########.fr       */
+/*   Updated: 2025/11/27 16:04:51 by surayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DIRECTORY_H
 # define DIRECTORY_H
 
+# include <constants.h>
+# include <dirent.h>
+# include <errno.h>
 # include <libft.h>
 # include <stdbool.h>
+# include <stdlib.h>
+# include <sys/stat.h>
 
-int		get_directory_entries(const char *path, bool include_hidden,
-			t_list **entries);
+int	get_directory_entries(const char *path, bool include_hidden,
+		t_list **entries);
 
 #endif
