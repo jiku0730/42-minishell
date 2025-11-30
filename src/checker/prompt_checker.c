f893fc2e512191b9a5aacf2ad982d4560101a6d9
jiku0730 <kei2003730@gmail.com>
2025-12-01 01:52:51 +0900

    add: compile command

--- src/checker/prompt_checker.c
+++ src/checker/prompt_checker.c
@@ -6,13 +6,14 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 01:45:50 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/12/01 01:47:33 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2025/12/01 01:52:26 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <constants.h>
 #include <stdio.h>
 
+/* $>cc ./prompt_checker.c -I../../includes */
 int	main(void)
 {
 	printf("SHELL_NAME = %s\n", SHELL_NAME);
