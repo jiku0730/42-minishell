26979b8003f30735b737baaddf173200b3077ce2
urassh <shuto.1011@icloud.com>
2025-12-01 14:46:45 +0900

    fix: checker

--- src/checker/path_checker.c
+++ src/checker/path_checker.c
@@ -13,8 +13,8 @@
 #include "minishell.h"
 #include "path.h"
 #include <readline/readline.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 void	path_checker(char *input, t_shell_table *shell_table)
 {
@@ -23,24 +23,22 @@ void	path_checker(char *input, t_shell_table *shell_table)
 	(void)shell_table;
 	if (ft_strncmp(input, "exit", 5) == 0)
 	{
-		write(STDOUT_FILENO, "exit\n", 5);
+		printf("exit\n");
 		rl_clear_history();
 		free(input);
 		exit(0);
 	}
-	ft_putstr_fd("\n=== Path Checker ===\n", 1);
-	ft_putstr_fd("Input: ", 1);
-	ft_putendl_fd(input, 1);
+	printf("\n=== Path Checker ===\n");
+	printf("Input: %s\n", input);
 	absolute_path = to_absolute(input);
 	if (absolute_path)
 	{
-		ft_putstr_fd("Absolute Path: ", 1);
-		ft_putendl_fd(absolute_path, 1);
+		printf("Absolute Path: %s\n", absolute_path);
 		free(absolute_path);
 	}
 	else
 	{
-		ft_putstr_fd("Error: Failed to convert to absolute path\n", 1);
+		ft_putendl_fd("Error: Failed to convert to absolute path", 2);
 	}
 	free(input);
 }
