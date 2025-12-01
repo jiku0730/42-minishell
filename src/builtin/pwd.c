f82e6b837bc8f3287775ca814c5b1f701a4ea122
urassh <shuto.1011@icloud.com>
2025-12-01 15:13:36 +0900

    fix: src/builtin/pwd.c

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: surayama <surayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 20:16:00 by surayama          #+#    #+#             */
-/*   Updated: 2025/12/01 15:08:42 by surayama         ###   ########.fr       */
+/*   Updated: 2025/12/01 15:13:22 by surayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	pwd(t_list *argv, t_shell_table *shell_table)
 	if (!pwd_path)
 	{
 		perror("pwd");
-		return (ERROR);
+		return (EXIT_FAILURE);
 	}
 	printf("%s\n", pwd_path);
 	free(pwd_path);
-	return (SUCCESS);
+	return (EXIT_SUCCESS);
 }
