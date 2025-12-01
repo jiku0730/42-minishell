02de13d148bd8bd679b73962432ee7f91bc91730
urassh <shuto.1011@icloud.com>
2025-12-01 15:08:55 +0900

    fix: pwdでshellテーブル検索を削除

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: surayama <surayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 20:16:00 by surayama          #+#    #+#             */
-/*   Updated: 2025/11/29 20:29:40 by surayama         ###   ########.fr       */
+/*   Updated: 2025/12/01 15:08:42 by surayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,7 @@ int	pwd(t_list *argv, t_shell_table *shell_table)
 	char	*pwd_path;
 
 	(void)argv;
-	pwd_path = st_search(shell_table, "PWD");
-	if (pwd_path)
-	{
-		printf("%s\n", pwd_path);
-		return (SUCCESS);
-	}
+	(void)shell_table;
 	pwd_path = getcwd(NULL, 0);
 	if (!pwd_path)
 	{
