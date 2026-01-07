79c9693d4d58c8b223daf3226077b3c8228919aa
jiku0730 <kei2003730@gmail.com>
2026-01-07 21:12:13 +0900

    chore: header fileの括弧をダブルコーテーションに変更した。

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: surayama <surayama@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 20:16:00 by surayama          #+#    #+#             */
-/*   Updated: 2025/12/01 15:13:22 by surayama         ###   ########.fr       */
+/*   Updated: 2026/01/07 21:11:50 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <builtin.h>
+#include "builtin.h"
 
 int	pwd(t_list *argv, t_shell_table *shell_table)
 {
