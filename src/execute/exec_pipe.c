7d60ab3cee3e460dd534bdeba3ede764fd573e19
jiku0730 <kei2003730@gmail.com>
2026-01-08 10:20:10 +0900

    change: exec_pipe function and fix int -> static int

--- src/execute/exec_pipe.c
+++ src/execute/exec_pipe.c
@@ -6,19 +6,18 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 05:46:56 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/01/08 10:10:41 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/01/08 10:19:25 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 
-int	exec_ast(t_ast *node, t_shell_table *shell_table);
-
-int	exec_pipe_left(t_ast *node, char *const envp[], pid_t *left, int fd[2])
+static int	exec_pipe_left(t_ast *node, t_shell_table *shell_table, \
+				pid_t *left, int fd[2])
 {
 	*left = fork();
 	if (*left == 0)
-		exec_left_child(node, envp, fd);
+		exec_left_child(node, shell_table, fd);
 	else if (*left < 0)
 	{
 		close(fd[0]);
@@ -28,11 +27,12 @@ int	exec_pipe_left(t_ast *node, char *const envp[], pid_t *left, int fd[2])
 	return (0);
 }
 
-int	exec_pipe_right(t_ast *node, char *const envp[], pid_t *right, int fd[2])
+static int	exec_pipe_right(t_ast *node, t_shell_table *shell_table, \
+				pid_t *right, int fd[2])
 {
 	*right = fork();
 	if (*right == 0)
-		exec_right_child(node, envp, fd);
+		exec_right_child(node, shell_table, fd);
 	else if (*right < 0)
 	{
 		close(fd[0]);
@@ -52,9 +52,9 @@ int	exec_pipe(t_ast *node, t_shell_table *shell_table)
 
 	if (pipe(fd) == -1)
 		return (1);
-	if (exec_pipe_left(node, envp, &left, fd) != 0)
+	if (exec_pipe_left(node, shell_table, &left, fd) != 0)
 		return (1);
-	if (exec_pipe_right(node, envp, &right, fd) != 0)
+	if (exec_pipe_right(node, shell_table, &right, fd) != 0)
 	{
 		if (left > 0)
 			waitpid(left, NULL, 0);
