5a242dc57ff724200e7b3c3bfd190bb73ba637ea
urassh <shuto.1011@icloud.com>
2026-02-01 23:26:37 +0900

    quoteのエラー修正

--- src/component/variable_expand/state/in_double_quote.c
+++ src/component/variable_expand/state/in_double_quote.c
@@ -14,6 +14,7 @@
 #include "variable_expand_internal.h"
 
 static void	by_dollar(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current);
+static void	by_double_quote_end(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current);
 
 void		expand_in_double_quote(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current)
 {
@@ -21,14 +22,22 @@ void		expand_in_double_quote(t_shell_table *shell_table, t_expand_store *store, 
 		*state = EXPAND_ON_ERROR;
 	else if (current == '$')
 		by_dollar(shell_table, store, state, current);
+	else if (current == '"')
+		by_double_quote_end(shell_table, store, state, current);
 	else if (expand_add_buffer(store, current) == ERROR)
 		*state = EXPAND_ON_ERROR;
-	else if (current == '"')
-		*state = EXPAND_IN_NORMAL;
 }
 
 static void	by_dollar(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current)
 {
 	if (expand_push_token(store, shell_table) == ERROR || expand_add_buffer(store, current) == ERROR)
 		*state = EXPAND_ON_ERROR;
+}
+
+static void	by_double_quote_end(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current)
+{
+	if (expand_push_token(store, shell_table) == ERROR || expand_add_buffer(store, current) == ERROR)
+		*state = EXPAND_ON_ERROR;
+	else
+		*state = EXPAND_IN_NORMAL;
 }
--- src/component/variable_expand/state/in_single_quote.c
+++ src/component/variable_expand/state/in_single_quote.c
@@ -13,22 +13,22 @@
 #include "variable_expand.h"
 #include "variable_expand_internal.h"
 
-static void	by_dollar(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current);
+static void	by_single_quote_end(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current);
 
 void		expand_in_single_quote(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current)
 {
 	if (current == '\0')
 		*state = EXPAND_ON_ERROR;
-	else if (current == '$')
-		by_dollar(shell_table, store, state, current);
+	else if (current == '\'')
+		by_single_quote_end(shell_table, store, state, current);
 	else if (expand_add_buffer(store, current) == ERROR)
 		*state = EXPAND_ON_ERROR;
-	else if (current == '"')
-		*state = EXPAND_IN_NORMAL;
 }
 
-static void	by_dollar(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current)
+static void	by_single_quote_end(t_shell_table *shell_table, t_expand_store *store, t_expand_state *state, char current)
 {
 	if (expand_push_token(store, shell_table) == ERROR || expand_add_buffer(store, current) == ERROR)
 		*state = EXPAND_ON_ERROR;
+	else
+		*state = EXPAND_IN_NORMAL;
 }
