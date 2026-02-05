d34c1b6a2b07e2cf7c87f60205df0dcc4c5bafb8
urassh <84073765+urassh@users.noreply.github.com>
2026-02-05 19:27:42 +0900

    tokenizeを全ての演算子単位で分割するように (#49)
    * fix: ()の定義をoperatorから分割するように
    
    * fix: あらゆる演算子に対応するように
    
    * add: LESSGREAT
    
    * refactor: 残っている意図が伝わる条件に
    
    * refactor: operator
    
    * chore: resultを削除
    
    * if文の定義をまとめた
    
    * fix: if
    
    * fix: private tokenize
    
    * delete: internal header

---
+++ includes/remove_quotes.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   remove_quote.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: surayama <surayama@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 00:50:48 by surayama          #+#    #+#             */
+/*   Updated: 2026/02/07 00:51:19 by surayama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REMOVE_QUOTES_H
+# define REMOVE_QUOTES_H
+
+# include "libft.h"
+
+t_list	*remove_quotes(t_list *tokens);
+
+#endif
