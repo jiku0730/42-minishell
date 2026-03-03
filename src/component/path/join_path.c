6f8e8fb508fb97cf726de1f318382e1aaf8ee15b
jiku0730 <kei2003730@gmail.com>
2026-03-03 17:00:45 +0900

    fix: join_pathの引数名を変更。path -> path_before, cmd -> path_after.

--- src/component/path/join_path.c
+++ src/component/path/join_path.c
@@ -6,25 +6,25 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 16:34:11 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/03/03 16:44:45 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/03/03 16:57:28 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "path.h"
 
-char	*join_path(const char *path, const char *cmd)
+char	*join_path(const char *path_before, const char *path_after)
 {
 	size_t	path_len;
 	size_t	cmd_len;
 	char	*joined;
 
-	path_len = ft_strlen(path);
-	cmd_len = ft_strlen(cmd);
+	path_len = ft_strlen(path_before);
+	cmd_len = ft_strlen(path_after);
 	joined = malloc(sizeof(char) * (path_len + 1 + cmd_len + 1));
 	if (!joined)
 		return (NULL);
-	ft_strlcpy(joined, path, path_len + 1);
+	ft_strlcpy(joined, path_before, path_len + 1);
 	joined[path_len] = '/';
-	ft_strlcpy(joined + path_len + 1, cmd, cmd_len + 1);
+	ft_strlcpy(joined + path_len + 1, path_after, cmd_len + 1);
 	return (joined);
 }
