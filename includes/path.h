fd419ec7aa5d73e4938bbe41f441fc57b6ac9c3e
jiku <134150757+jiku0730@users.noreply.github.com>
2026-03-03 17:02:17 +0900

    add: join_path.cをsrc/components/pathに追加しました。 (#59)
    * add: join_path.c
    
    * fix: join_path関数の返り値の型からstaticを削除。
    
    * update: makefileにsrc/components/path/join_path.cを追加。
    
    * update: path.hにjoin_path関数のプロトタイプ宣言を追加。
    
    * fix: join_pathの引数名を変更。path -> path_before, cmd -> path_after.
    
    ---------
    
    Co-authored-by: jiku0730 <kei2003730@gmail.com>

--- includes/path.h
+++ includes/path.h
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 00:00:00 by surayama          #+#    #+#             */
-/*   Updated: 2026/01/16 13:59:51 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/03/03 16:57:47 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@
 char	*to_absolute(const char *path);
 t_list	*append_path(t_list **dest, const char *content);
 t_list	*append_path_list(t_list **dest, const char *content);
+char	*join_path(const char *path_before, const char *path_after);
 
 #endif
