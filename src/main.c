197e3020a975d9f98e47bf47d42bee41b8d8f001
urassh <84073765+urassh@users.noreply.github.com>
2026-04-06 07:27:27 +0900

    全チェッカーを CLI フラグで起動可能にする (#65)

--- src/main.c
+++ src/main.c
@@ -6,18 +6,26 @@
 /*   By: surayama <surayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 15:13:47 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/03/03 19:17:28 by surayama         ###   ########.fr       */
+/*   Updated: 2026/03/03 20:42:40 by surayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main_prod(int argc, char const **argv, char *const envp[])
+static t_checker	find_checker(const char *arg);
+static int			run_checker(t_checker checker, char *const envp[]);
+
+int	main(int argc, char const **argv, char *const envp[])
 {
 	t_shell_table	*shell_table;
+	t_checker		checker;
 
-	(void)argc;
-	(void)argv;
+	if (argc > 1)
+	{
+		checker = find_checker(argv[1]);
+		if (checker)
+			return (run_checker(checker, envp));
+	}
 	shell_table = build_shell_table(envp);
 	if (!shell_table)
 		return (1);
@@ -26,25 +34,41 @@ int	main_prod(int argc, char const **argv, char *const envp[])
 	return (0);
 }
 
-int	main_dev(int argc, char const **argv, char *const envp[])
+static t_checker	find_checker(const char *arg)
+{
+	static const t_checker_entry	g[] = {
+	{"--tokenize", tokenize_checker},
+	{"--heredoc", heredoc_checker},
+	{"--parse", parse_checker},
+	{"--directory", directory_checker},
+	{"--path", path_checker},
+	{"--builtin", builtin_checker},
+	{"--expand", expand_checker},
+	{"--remove-quotes", remove_quotes_checker},
+	{"--wildcard", wildcard_checker},
+	{"--relative-path", relative_path_checker},
+	{NULL, NULL}
+	};
+	int								i;
+
+	i = 0;
+	while (g[i].name)
+	{
+		if (ft_strncmp(arg, g[i].name, ft_strlen(g[i].name) + 1) == 0)
+			return (g[i].checker);
+		i++;
+	}
+	return (NULL);
+}
+
+static int	run_checker(t_checker checker, char *const envp[])
 {
 	t_shell_table	*shell_table;
 
-	(void)argc;
-	(void)argv;
 	shell_table = build_shell_table(envp);
 	if (!shell_table)
 		return (1);
-	shell_table_checker(shell_table);
-	prompt(expand_checker, shell_table);
+	prompt(checker, shell_table);
 	st_destroy(shell_table);
 	return (0);
-}
-
-int	main(int argc, char const **argv, char *const envp[])
-{
-	if (argc > 1 && ft_strncmp(argv[1], "--dev", 6) == 0)
-		return (main_dev(argc, argv, envp));
-	else
-		return (main_prod(argc, argv, envp));
 }
