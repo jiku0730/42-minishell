83391bcaf793ff032332a46103a3235a3d805e07
urassh <shuto.1011@icloud.com>
2026-04-16 17:16:58 +0900

    add: on_inputを本番用に組み替え

--- src/callback/on_input.c
+++ src/callback/on_input.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:04:25 by surayama          #+#    #+#             */
-/*   Updated: 2026/01/16 13:59:52 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/04/16 00:00:00 by surayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,24 @@
 
 void	on_input(char *input, t_shell_table *shell_table)
 {
-	(void)shell_table;
-	free(input);
+	t_list	*tokens;
+	t_ast	*ast;
+
+	tokens = tokenize(input);
+	if (!tokens)
+		return ;
+	if (!heredoc(tokens))
+	{
+		ft_lstclear(&tokens, free);
+		return ;
+	}
+	tokens = expand(tokens, shell_table);
+	if (!tokens)
+		return ;
+	ast = parse(tokens);
+	ft_lstclear(&tokens, free);
+	if (!ast)
+		return ;
+	exec_ast(ast, shell_table);
+	free_ast(ast);
 }
