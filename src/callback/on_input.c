7a6e2e88cbd41ea1b64676d6d03413782df3a655
jiku0730 <github@jiku.jp>
2026-04-17 21:44:30 +0900

    fix: コンフリクトを解消。

--- src/callback/on_input.c
+++ src/callback/on_input.c
@@ -6,36 +6,17 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:04:25 by surayama          #+#    #+#             */
-/*   Updated: 2026/04/17 21:23:54 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/04/17 21:43:48 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD
 int	on_input(char *input, t_shell_table *shell_table, int last_status)
 {
 	t_list	*tokens;
 	t_ast	*ast;
 	int		current_status;
-=======
-static void	update_exit_status(t_shell_table *shell_table, int status)
-{
-	char	*status_str;
-
-	status_str = ft_itoa(status);
-	if (!status_str)
-		return ;
-	st_insert(shell_table, "?", status_str, false);
-	free(status_str);
-}
-
-void	on_input(char *input, t_shell_table *shell_table)
-{
-	t_list	*tokens;
-	t_ast	*ast;
-	int		status;
->>>>>>> 5796375 (wip: exec_ast の戻り値を $? として shell_table に保存)
 
 	tokens = tokenize(input);
 	if (!tokens)
@@ -51,15 +32,8 @@ void	on_input(char *input, t_shell_table *shell_table)
 	ast = parse(tokens);
 	ft_lstclear(&tokens, free);
 	if (!ast)
-<<<<<<< HEAD
 		return (2);
 	current_status = exec_ast(ast, shell_table);
 	free_ast(ast);
 	return (current_status);
-=======
-		return ;
-	status = exec_ast(ast, shell_table);
-	free_ast(ast);
-	update_exit_status(shell_table, status);
->>>>>>> 5796375 (wip: exec_ast の戻り値を $? として shell_table に保存)
 }
