eaae67f14b193e8b882543130286e4b5dff707e9
jiku0730 <github@jiku.jp>
2026-04-17 21:24:23 +0900

    chore: 可読性を上げるために、current_statusという一時変数を追加。

--- src/callback/on_input.c
+++ src/callback/on_input.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:04:25 by surayama          #+#    #+#             */
-/*   Updated: 2026/04/17 21:23:20 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/04/17 21:23:54 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	on_input(char *input, t_shell_table *shell_table, int last_status)
 {
 	t_list	*tokens;
 	t_ast	*ast;
+	int		current_status;
 
 	tokens = tokenize(input);
 	if (!tokens)
@@ -32,7 +33,7 @@ int	on_input(char *input, t_shell_table *shell_table, int last_status)
 	ft_lstclear(&tokens, free);
 	if (!ast)
 		return (2);
-	last_status = exec_ast(ast, shell_table);
+	current_status = exec_ast(ast, shell_table);
 	free_ast(ast);
-	return (last_status);
+	return (current_status);
 }
