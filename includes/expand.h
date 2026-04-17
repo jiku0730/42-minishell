ede8631ace08541d4a1da2b8d19a90dba91a60e7
jiku0730 <github@jiku.jp>
2026-04-17 21:32:03 +0900

    chore: expand層とHeaderの関数の順番を合わせた。

--- includes/expand.h
+++ includes/expand.h
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 17:13:55 by surayama          #+#    #+#             */
-/*   Updated: 2026/04/17 21:11:14 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/04/17 21:31:58 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@
 # include <stdbool.h>
 
 t_list	*expand(t_list *tokens, t_shell_table *shell_table, int last_status);
-t_list	*expand_parameter(t_list *tokens, t_shell_table *shell_table);
 t_list	*expand_tilde(t_list *tokens, t_shell_table *shell_table);
+t_list	*expand_question(t_list *tokens, int last_exit_status);
+t_list	*expand_parameter(t_list *tokens, t_shell_table *shell_table);
 t_list	*expand_wildcard(t_list *tokens);
 t_list	*expand_remove_quotes(t_list *tokens);
 t_list	*expand_question(t_list *tokens, int last_exit_status);
