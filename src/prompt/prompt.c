9091427ddb869c5f815cbc318eb4147ecd839c9c
jiku0730 <github@jiku.jp>
2026-04-18 17:13:28 +0900

    fix: prompt状態でSIGINTを受け取った時に、終了ステータスが130になるように修正。

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,12 +6,22 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:35:00 by surayama          #+#    #+#             */
-/*   Updated: 2026/04/17 21:22:08 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/04/18 17:06:01 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prompt.h"
 #include "signal_handler.h"
+
+static int	apply_sigint_status(int last_status)
+{
+	if (g_signal == SIGINT)
+	{
+		g_signal = 0;
+		return (130);
+	}
+	return (last_status);
+}
 
 int	prompt(int (*handler)(char *input, t_shell_table *shell_table,
 			int last_status), t_shell_table *shell_table)
@@ -23,6 +33,7 @@ int	prompt(int (*handler)(char *input, t_shell_table *shell_table,
 	while (1)
 	{
 		input = readline(PROMPT);
+		last_status = apply_sigint_status(last_status);
 		if (!input)
 		{
 			write(STDOUT_FILENO, "exit\n", 5);
